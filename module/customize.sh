1051fe0671d10b173077038829988986daf47850
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-10 22:53:18 +0800

    scripts/customize: migrate old config on reinstall

--- module/customize.sh
+++ module/customize.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
+# customize.sh
 
 if ! grep -q overlay /proc/filesystems > /dev/null 2>&1; then \
 	abort "[!] OverlayFS is required for this module!"
 fi
 
+configs="modules.txt whiteouts.txt"
+for file in $configs; do
+	if [ -f "/data/adb/modules/mountify/$file" ]; then
+		echo "[+] migrating $file"
+		cat "/data/adb/modules/mountify/$file" > "$MODPATH/$file"
+	fi
+done
+
+# EOF
