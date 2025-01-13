8c9dec5abdbe490e39fff6c1934bdbaba1cdd146
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-13 19:21:37 +0800

    scripts/mount: drop vendor mimic

--- module/mount.sh
+++ module/mount.sh
@@ -3,9 +3,9 @@
 # global mounting script for ap/ksu modules via overlayfs
 
 # exit for missing args
-if [ -z $1 ] || [ -z $2 ]; then
+if [ -z $1 ]; then
 	echo "usage: "
-	echo "$(basename "$0" ) module_id fake_folder_name"
+	echo "$(basename "$0" ) module_id"
 	exit 1
 fi
 
@@ -21,37 +21,20 @@ if [ -f $TARGET_DIR/disable ] || [ -f $TARGET_DIR/remove ]; then
 	exit 1
 fi
 
-FAKE_MOUNT_NAME="$2"
 SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 [ ! -f $TARGET_DIR/skip_mount ] && touch $TARGET_DIR/skip_mount
-
-[ -w /mnt ] && MNT_FOLDER=/mnt
-[ -w /mnt/vendor ] && MNT_FOLDER=/mnt/vendor
-
-mkdir "$MNT_FOLDER/$FAKE_MOUNT_NAME"
-
-if [ ! -d "$MNT_FOLDER/$FAKE_MOUNT_NAME" ]; then
-	echo "failed creating folder with fake_folder_name $2"
-	exit 1
-fi
-
-${SUSFS_BIN} add_sus_path "$MNT_FOLDER/$FAKE_MOUNT_NAME"
 
 # handle single depth on magic mount
 single_depth() {
 	for DIR in $( ls -d system/apex/ system/app/ system/bin/ system/etc/ system/fonts/ system/framework/ system/lib/ system/lib64/ system/priv-app/ system/usr/ 2>/dev/null ); do
-		mkdir -p "$MNT_FOLDER/$FAKE_MOUNT_NAME/$DIR"
-		busybox mount --bind "$(pwd)/$DIR" "$MNT_FOLDER/$FAKE_MOUNT_NAME/$DIR"
-		busybox mount -t overlay -o "lowerdir=$MNT_FOLDER/$FAKE_MOUNT_NAME/$DIR:/$DIR" overlay "/$DIR"
+		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/$DIR" overlay "/$DIR"
 		${SUSFS_BIN} add_sus_mount "/$DIR"
 	done
 }
 
 normal_depth() {
 	for DIR in $(ls -d */*/ ); do
-		mkdir -p "$MNT_FOLDER/$FAKE_MOUNT_NAME/$DIR"
-		busybox mount --bind "$(pwd)/$DIR" "$MNT_FOLDER/$FAKE_MOUNT_NAME/$DIR"
-		busybox mount -t overlay -o "lowerdir=$MNT_FOLDER/$FAKE_MOUNT_NAME/$DIR:/$DIR" overlay "/$DIR"
+		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/$DIR" overlay "/$DIR"
 		${SUSFS_BIN} add_sus_mount "/$DIR"
 	done
 }
