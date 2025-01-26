af786c24ab7f26266389ea0f50cf2171e43ce659
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-26 14:59:50 +0800

    scripts/customize: test requirements

--- module/customize.sh
+++ module/customize.sh
@@ -1,8 +1,46 @@
 #!/bin/sh
+PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
 # customize.sh
 
-if ! grep -q overlay /proc/filesystems > /dev/null 2>&1; then \
-	abort "[!] OverlayFS is required for this module!"
+# some bullshit just to use clear
+if [ "$MMRL" = "true" ] || { [ "$KSU" = "true" ] && [ "$KSU_VER_CODE" -ge 11998 ]; } || 
+	{ [ "$KSU_NEXT" = "true" ] && [ "$KSU_VER_CODE" -ge 12144 ]; } ||
+	{ [ "$APATCH" = "true" ] && [ "$APATCH_VER_CODE" -ge 11022 ]; }; then
+	clear
+        loops=20
+        while [ $loops -gt 1 ];  do 
+		for i in '[-]' '[/]' '[|]' '[\]'; do 
+		        echo "$i"
+		        sleep 0.1
+		        clear
+		        loops=$((loops - 1)) 
+		done
+        done
+else
+	# sleep a bit to make it look like something is happening!!
+	sleep 2
+fi
+
+echo "[+] mountify"
+echo "[+] SysReq test"
+
+# test for overlayfs
+if grep -q "overlay" /proc/filesystems > /dev/null 2>&1; then \
+	echo "[+] CONFIG_OVERLAY_FS"
+	echo "[+] overlay found in /proc/filesystems"
+else
+	abort "[!] CONFIG_OVERLAY_FS is required for this module!"
+fi
+
+# test for tmpfs xattr
+testfile="/debug_ramdisk/tmpfs_xattr_testfile"
+rm $testfile > /dev/null 2>&1 
+busybox mknod "$testfile" c 0 0 > /dev/null 2>&1 
+if busybox setfattr -n trusted.overlay.whiteout -v y "$testfile" > /dev/null 2>&1 ; then 
+	echo "[+] CONFIG_TMPFS_XATTR"
+	echo "[+] tmpfs extended attribute test passed"
+else
+	abort "[!] CONFIG_TMPFS_XATTR is required for this module!"
 fi
 
 configs="modules.txt whiteouts.txt config.sh"
