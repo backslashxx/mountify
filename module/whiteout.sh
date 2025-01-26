4577736c26e1c7b8ef7b4f486042ac9968051f19
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-26 22:15:50 +0800

    scripts/whiteout: double down on whiteout routine

--- module/whiteout.sh
+++ module/whiteout.sh
@@ -11,6 +11,24 @@ if [ $mountify_whiteouts = 0 ]; then
 	exit 0
 fi
 
+# functions
+# whiteout_create
+whiteout_create() {
+	mkdir -p "/debug_ramdisk/mountify/wo/${1%/*}"
+  	busybox mknod "/debug_ramdisk/mountify/wo/$1" c 0 0
+  	busybox chcon --reference="$1" "/debug_ramdisk/mountify/wo/$1"  
+  	busybox setfattr -n trusted.overlay.whiteout -v y "/debug_ramdisk/mountify/wo/$1"
+  	chmod 644 "/debug_ramdisk/mountify/wo/$1"
+}
+
+for line in $( sed '/#/d' "$MODDIR/whiteouts.txt" ); do
+	# make sure to only whiteout if file exists
+	if [ -e "$line" ]; then
+		echo "mountify/whiteout: whiting-out $line " >> /dev/kmsg
+		whiteout_create "$line"
+	fi
+done
+
 # here we do the vendor mount mimic
 [ -w /mnt ] && MNT_FOLDER=/mnt
 [ -w /mnt/vendor ] && MNT_FOLDER=/mnt/vendor
@@ -22,7 +40,7 @@ if [ -d "$MNT_FOLDER/$FAKE_MOUNT_NAME" ]; then
 fi
 
 # create it
-mkdir -p "$MNT_FOLDER/$FAKE_MOUNT_NAME"
+cd "$MNT_FOLDER" && cp -r "/debug_ramdisk/mountify/wo" "$FAKE_MOUNT_NAME"
 
 # then make sure its there
 if [ ! -d "$MNT_FOLDER/$FAKE_MOUNT_NAME" ]; then
@@ -30,31 +48,16 @@ if [ ! -d "$MNT_FOLDER/$FAKE_MOUNT_NAME" ]; then
 	exit 1
 fi
 
-# functions
-# whiteout_create
-whiteout_create() {
-	mkdir -p "$MNT_FOLDER/$FAKE_MOUNT_NAME/${1%/*}"
-  	busybox mknod "$MNT_FOLDER/$FAKE_MOUNT_NAME/$1" c 0 0
-  	busybox chcon --reference="$1" "$MNT_FOLDER/$FAKE_MOUNT_NAME/$1"  
-  	busybox setfattr -n trusted.overlay.whiteout -v y "$MNT_FOLDER/$FAKE_MOUNT_NAME/$1"
-  	chmod 644 "$MNT_FOLDER/$FAKE_MOUNT_NAME/$1"
-}
+cd "$MNT_FOLDER/$FAKE_MOUNT_NAME"
 
-for line in $( sed '/#/d' "$MODDIR/whiteouts.txt" ); do
-	# make sure to only whiteout if file exists
-	if [ -e "$line" ]; then
-		echo "mountify/whiteout: whiting-out $line " >> /dev/kmsg
-		whiteout_create "$line"
-	fi
+for file in $( find ./ | sed "s|./|/|") ; do 
+	busybox chcon --reference="/debug_ramdisk/mountify/wo/$file" ".$file"  
 done
 
-if [ -d "$MNT_FOLDER/$FAKE_MOUNT_NAME" ]; then
-	echo "mountify/whiteout: mounting whiteouts" >> /dev/kmsg
-	cd "$MNT_FOLDER/$FAKE_MOUNT_NAME"
-	for DIR in $(ls -d */*/); do
-		busybox mount -t overlay -o "lowerdir=$MNT_FOLDER/$FAKE_MOUNT_NAME/$DIR:/$DIR" overlay "/$DIR"
-		${SUSFS_BIN} add_sus_mount "/$DIR"
-	done
-fi
+echo "mountify/whiteout: mounting whiteouts" >> /dev/kmsg
+for DIR in $(ls -d */*/); do
+	busybox mount -t overlay -o "lowerdir=$MNT_FOLDER/$FAKE_MOUNT_NAME/$DIR:/$DIR" overlay "/$DIR"
+	${SUSFS_BIN} add_sus_mount "/$DIR"
+done
 
 # EOF
