1b92ef8fac0e663b8fe7bc4c0a5e3fbcb60faeca
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-26 15:32:54 +0800

    scripts/mount: magisk support maybe
    Revert "module: drop magisk support"
    
    This reverts commit 91d4016898372b511ff84ff16936605288bc805a.

--- module/mount.sh
+++ module/mount.sh
@@ -94,7 +94,8 @@ single_depth() {
 # https://github.com/5ec1cff/KernelSU/commit/92d793d0e0e80ed0e87af9e39879d2b70c37c748
 # on overlayfs, moddir/system/product is symlinked to moddir/product
 # on magic, moddir/product it symlinked to moddir/system/product
-if [ "$KSU_MAGIC_MOUNT" = "true" ] || [ "$APATCH_BIND_MOUNT" = "true" ]; then
+if [ "$KSU_MAGIC_MOUNT" = "true" ] || [ "$APATCH_BIND_MOUNT" = "true" ] || \
+	{ [ -f /data/adb/magisk/magisk ] && [ -z "$KSU" ] && [ -z "$APATCH" ]; }; then
 	# handle single depth on magic mount
 	single_depth
 	# normal routine
