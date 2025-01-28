d1954ec3d73ff14533c3ca70188b8cb1c1f93ce4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-28 21:04:34 +0800

    module: whiteout: import resources

---
+++ module/whiteout/action.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# action.sh
+# this script is part of mountify
+# No warranty.
+# No rights reserved.
+# This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
+MODDIR="${0%/*}"
+
+echo "[+] mountify"
+echo "[+] whiteouts"
+printf "\n\n"
+
+if [ -d $MODDIR/system ]; then
+	busybox tree $MODDIR/system
+fi
+
+# ksu and apatch auto closes
+# make it wait 20s so we can read
+if [ -z "$MMRL" ] && { [ "$KSU" = "true" ] || [ "$APATCH" = "true" ]; }; then
+	sleep 20
+fi
+
+# EOF
