55f6231d4209e816231fc93c3c836e9a3ae43670
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-01-29 07:25:51 +0800

    scripts/mount: handle mountify_whiteout as magic always
    since it follows magic mount folder scheme where everything is on /system/

--- module/mount.sh
+++ module/mount.sh
@@ -127,7 +127,7 @@ single_depth() {
 # https://github.com/5ec1cff/KernelSU/commit/92d793d0e0e80ed0e87af9e39879d2b70c37c748
 # on overlayfs, moddir/system/product is symlinked to moddir/product
 # on magic, moddir/product it symlinked to moddir/system/product
-if [ "$KSU_MAGIC_MOUNT" = "true" ] || [ "$APATCH_BIND_MOUNT" = "true" ] || { [ -f /data/adb/magisk/magisk ] && [ -z "$KSU" ] && [ -z "$APATCH" ]; }; then
+if [ "$KSU_MAGIC_MOUNT" = "true" ] || [ "$APATCH_BIND_MOUNT" = "true" ] || { [ -f /data/adb/magisk/magisk ] && [ -z "$KSU" ] && [ -z "$APATCH" ]; } || [ "$MODULE_ID" = "mountify_whiteouts" ]; then
 	# handle single depth on magic mount
 	single_depth
 	# handle this stance when /product is a symlink to /system/product
