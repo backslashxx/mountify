1720333482ea0f0bea3f1a45f631ddb36b31b86a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-01 19:47:44 +0800

    scripts/post-fs-data: remove trailing /

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -26,7 +26,7 @@ vendor"
 # functions
 # normal depth
 normal_depth() {
-	for DIR in $(ls -d */*/); do
+	for DIR in $(ls -d */* ); do
 		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/$DIR" overlay "/$DIR"
 		${SUSFS_BIN} add_sus_mount "/$DIR"
 	done
@@ -34,7 +34,7 @@ normal_depth() {
 
 # controlled depth
 controlled_depth() {
-	for DIR in $(ls -d $1/*/); do
+	for DIR in $(ls -d $1/* ); do
 		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/$DIR" overlay "/$DIR"
 		${SUSFS_BIN} add_sus_mount "/$DIR"
 	done
@@ -42,7 +42,7 @@ controlled_depth() {
 
 # handle single depth on magic mount
 single_depth() {
-	for DIR in $( ls -d system/*/ | grep -vE "odm|product|system_ext|vendor" 2>/dev/null ); do
+	for DIR in $( ls -d system/* | grep -vE "odm|product|system_ext|vendor" 2>/dev/null ); do
 		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/$DIR" overlay "/$DIR"
 		${SUSFS_BIN} add_sus_mount "/$DIR"
 	done
