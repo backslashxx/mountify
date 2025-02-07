c2ba45da738d1715dca8bda80480697942f66461
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-07 10:20:27 +0800

    scripts/post-fs-data: remove whiteout_addond
    this has issues when whited out anyway

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -8,7 +8,6 @@ PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
 SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 MODDIR="/data/adb/modules/mountify"
 mountify_mounts=1
-mountify_whiteout_addond=0
 
 # grab start time
 echo "mountify/post-fs-data: start!" >> /dev/kmsg
@@ -162,31 +161,6 @@ for line in $( sed '/#/d' "$MODDIR/modules.txt" ); do
 	folder_name=$( echo $line | awk {'print $2'} )
 	mountify "$module_id" "$folder_name"
 done
-
-# whiteout /system/addon.d
-# everything else can be handled like a module but not this, due 
-# to it being single depth. we can treat this as special case.
-whiteout_addond() {
-	if [ ! -e /system/addon.d ] || [ ! "$mountify_whiteout_addond" = 1 ] || 
-		[ -z "$FAKE_ADDOND_MOUNT_NAME" ] || [ -d "$MNT_FOLDER/$FAKE_ADDOND_MOUNT_NAME" ] ; then
-		return
-	fi
-	echo "mountify/post-fs-data: whiteout_addond routine start! " >> /dev/kmsg
-	# whiteout routine
-	addond_mount_point="$MNT_FOLDER/$FAKE_ADDOND_MOUNT_NAME/system"
-	mkdir -p "$addond_mount_point"
-	busybox chcon --reference="/system" "$addond_mount_point" 
-	busybox mknod "$addond_mount_point/addon.d" c 0 0
-	busybox chcon --reference="/system/addon.d" "$addond_mount_point/addon.d" 
-	busybox setfattr -n trusted.overlay.whiteout -v y "$addond_mount_point/addon.d"
-	chmod 644 "$addond_mount_point/addon.d"
-	
-	# mount
-	busybox mount -t overlay -o "lowerdir=$addond_mount_point:/system" overlay "/system"
-	[ ! -e /system/addon.d ] && echo "mountify/post-fs-data: whiteout_addond success!" >> /dev/kmsg
-}
-
-whiteout_addond
 
 echo "mountify/post-fs-data: finished!" >> /dev/kmsg
 
