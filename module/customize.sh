803386e80d6521d6da30eca6202829d22db485a8
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-11 17:03:00 +0800

    scripts/customize: do NOT migrate old config if older ver again

--- module/customize.sh
+++ module/customize.sh
@@ -72,9 +72,9 @@ else
 	mountify_versionCode=0
 fi
 
-# replace if 128 and older
-# https://github.com/backslashxx/mountify/commit/92fc25912da9a67097a8d802c2e7cb55cda2e658
-if [ $mountify_versionCode -gt 128 ]; then
+# replace if 129 and older
+# https://github.com/backslashxx/mountify/commit/caa2cfa1058e1f428e47047d057fa73fed3351ca
+if [ $mountify_versionCode -gt 129 ]; then
 	configs="modules.txt whiteouts.txt config.sh"
 else
 	echo "[!] config.sh will be replaced!"
