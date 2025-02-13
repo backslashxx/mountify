e9b3ab5eaeeb88bf9f3318decfa0f675073665f9
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-13 23:43:12 +0800

    script: make susfs usage opt-in

--- global_mount.sh
+++ global_mount.sh
@@ -19,6 +19,8 @@ FAKE_MOUNT_NAME="my_bullshit"
 
 # susfs usage is not required but we can use it if its there.
 SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
+# set to 1 to enable
+mountify_use_susfs=0
 
 # separate shit with lines
 IFS="
@@ -36,7 +38,7 @@ vendor"
 normal_depth() {
 	for DIR in $(ls -d */* ); do
 		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/$DIR" overlay "/$DIR"
-		${SUSFS_BIN} add_sus_mount "/$DIR"
+		[ $mountify_use_susfs = 1 ] && ${SUSFS_BIN} add_sus_mount "/$DIR"
 	done
 }
 
@@ -45,7 +47,7 @@ controlled_depth() {
 	if [ -z "$1" ] || [ -z "$2" ]; then return ; fi
 	for DIR in $(ls -d $1/* ); do
 		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:$2$DIR" overlay "$2$DIR"
-		${SUSFS_BIN} add_sus_mount "$2$DIR"
+		[ $mountify_use_susfs = 1 ] && ${SUSFS_BIN} add_sus_mount "$2$DIR"
 	done
 }
 
@@ -53,7 +55,7 @@ controlled_depth() {
 single_depth() {
 	for DIR in $( ls -d * | grep -vE "(odm|product|system_ext|vendor)$" 2>/dev/null ); do
 		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/system/$DIR" overlay "/system/$DIR"
-		${SUSFS_BIN} add_sus_mount "/system/$DIR"
+		[ $mountify_use_susfs = 1 ] && ${SUSFS_BIN} add_sus_mount "/system/$DIR"
 	done
 }
 
