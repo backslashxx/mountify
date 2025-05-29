068d75d807eca457d13cfb5b3bc365c8b8bd4c1e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-29 20:02:48 +0800

    module: WIP

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## standalone script
-- this is only meant for KernelSU OverlayFS
-- you can put this onto post-fs-data of modules that you want globally mounted.
-- heavily commented, read them.
-
+## mountify
+- symlink ver
+- requires non-casefolded /data/adb/modules
+- works
+- wip.
---
+++ module/post-fs-data.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+# post-fs-data.sh
+# this script is part of mountify (symlink ver)
+# No warranty.
+# No rights reserved.
+# This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
+PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
+MODDIR="/data/adb/modules/mountify"
+
+# this is a fast lookup for a writable dir
+# these tends to be always available
+[ -w /mnt ] && MNT_FOLDER=/mnt
+[ -w /mnt/vendor ] && MNT_FOLDER=/mnt/vendor
+
+IFS="
+"
+targets="odm
+product
+system_ext
+vendor"
+
+controlled_depth() {
+	if [ -z "$1" ] || [ -z "$2" ]; then return ; fi
+	for DIR in $(ls -d $1/*/ | sed 's/.$//' ); do
+		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:$2$DIR" overlay "$2$DIR"
+	done
+}
+
+single_depth() {
+	for DIR in $( ls -d */ | sed 's/.$//'  | grep -vE "^(odm|product|system_ext|vendor)$" 2>/dev/null ); do
+		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/system/$DIR" overlay /system/$DIR
+	done
+}
+
+mountify_symlink() {
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo "mountify/post-fs-data: missing arguments, fuck off" >> /dev/kmsg
+	return
+fi
+
+if [ "$1" = "bindhosts" ]; then
+	echo "mountify/post-fs-data: $1 blacklisted" >> /dev/kmsg
+	return
+fi	
+
+if [ -f "/data/adb/modules/$1/skip_mountify" ] || [ -f "/data/adb/modules/$1/disable" ] || [ -f "/data/adb/modules/$1/remove" ] || [ ! -d "/data/adb/modules/$1/system" ]; then
+	echo "mountify/post-fs-data: $1 not meant to be mounted" >> /dev/kmsg
+	return	
+fi
+
+MODULE_BASEDIR="/data/adb/modules/$1/system"
+FAKE_MOUNT_NAME="$2"
+	
+# here we create the symlink
+busybox ln -sf "$MODULE_BASEDIR" "$MNT_FOLDER/$FAKE_MOUNT_NAME"
+
+if [ ! -d "$MNT_FOLDER/$FAKE_MOUNT_NAME" ]; then
+	return
+fi
+cd "$MNT_FOLDER/$FAKE_MOUNT_NAME"
+
+# single_depth
+single_depth
+# controlled depth
+for folder in $targets ; do 
+	# reset cwd due to loop
+	cd "$MNT_FOLDER/$FAKE_MOUNT_NAME"
+	if [ -L "/$folder" ] && [ ! -L "/system/$folder" ]; then
+		# legacy, so we mount at /system
+		controlled_depth "$folder" "/system/"
+	else
+		# modern, so we mount at root
+		controlled_depth "$folder" "/"
+	fi
+done
+
+}
+
+# I dont think auto mode is possible right away
+# logic seems hard as we have to /mnt/vendor/module1/system/app:/mnt/vendor/module2/system/app
+# PR welcome if somebody sees a way to do it easily.
+# so ye manual for now
+
+# manual mode
+for line in $( sed '/#/d' "$MODDIR/modules.txt" ); do
+	module_id=$( echo $line | awk {'print $1'} )
+	mount_name=$( echo $line | awk {'print $2'} )
+	mountify_symlink "$module_id" "$mount_name"
+done
+
+# EOF
