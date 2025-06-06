a2f49c00c7165bf31eca9d88103cbac299ec30d0
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-06 22:11:43 +0800

    scripts/customize: add clear and overlayfs test

--- module/customize.sh
+++ module/customize.sh
@@ -6,9 +6,36 @@
 # This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
 PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
 
+# some bullshit just to use clear
+if [ "$MMRL" = "true" ] || { [ "$KSU" = "true" ] && [ "$KSU_VER_CODE" -ge 11998 ]; } || 
+	{ [ "$KSU_NEXT" = "true" ] && [ "$KSU_VER_CODE" -ge 12144 ]; } ||
+	{ [ "$APATCH" = "true" ] && [ "$APATCH_VER_CODE" -ge 11022 ]; }; then
+	clear
+        loops=20
+        while [ $loops -gt 1 ];  do 
+		for i in '[-]' '[/]' '[|]' '[\]'; do 
+		        echo "$i"
+		        sleep 0.1
+		        clear
+		        loops=$((loops - 1)) 
+		done
+        done
+else
+	# sleep a bit to make it look like something is happening!!
+	sleep 2
+fi
+
 # routine start
 echo "[+] mountify (symlink ver)"
 echo "[+] SysReq test"
+
+## test for overlayfs
+if grep -q "overlay" /proc/filesystems > /dev/null 2>&1; then \
+	echo "[+] CONFIG_OVERLAY_FS"
+	echo "[+] overlay found in /proc/filesystems"
+else
+	abort "[!] CONFIG_OVERLAY_FS is required for this module!"
+fi
 
 ## test for casefolding
 
@@ -41,7 +68,7 @@ else
 	abort "[x] files not created, casefolded"
 fi
 
-# need to do a test mount too
+## test mount
 busybox chcon --reference="/system" "$TEST_FOLDER"
 busybox chcon --reference="/system" "$TEST_FOLDER/CASEFOLD"
 busybox chcon --reference="/system" "$TEST_FOLDER/casefold"
