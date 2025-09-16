84b6aca909cfc27355daec35b071fe88696703e0
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-16 08:23:51 +0800

    scripts/boot-completed: default template for hiding
    by default these are disabled, so basically does nothing.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- module/boot-completed.sh
+++ module/boot-completed.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# service.sh
+# boot-completed.sh
 # this script is part of mountify
 # No warranty.
 # No rights reserved.
@@ -13,11 +13,34 @@ MODDIR="/data/adb/modules/mountify"
 # this script will be migrated by mountify re-installs / updates
 #
 
-# put your whatever in here
-# example
-# unmount my mounts via susfs
-# for mount in $(grep $MOUNT_DEVICE_NAME /proc/mounts | awk {'print $2'}) ; do 
-#	/data/adb/ksu/bin/ksu_susfs add_try_umount $mount 1
-# done
+[ -w /mnt ] && MNT_FOLDER=/mnt
+[ -w /mnt/vendor ] && MNT_FOLDER=/mnt/vendor
+
+# requires susfs add_try_umount
+do_susfs_hide() {
+for mount in $(grep "KSU" /proc/mounts | awk {'print $2'}) ; do 
+	/data/adb/ksu/bin/ksu_susfs add_try_umount $mount 1
+done
+}
+
+# requires modded ksud+driver with add-try-umount / nuke-ext4-sysfs
+# this will unregister sparse when ext4 is enabled and spoof_sparse is disabled!
+do_ksud_hide() {
+if { [ -f "$MODDIR/no_tmpfs_xattr" ] || [ "$use_ext4_sparse" = "1" ]; } && 
+	[ "$spoof_sparse" = "0" ]; then
+	/data/adb/ksud add-try-umount "$MNT_FOLDER/$FAKE_MOUNT_NAME"
+	/data/adb/ksud nuke-ext4-sysfs "$MNT_FOLDER/$FAKE_MOUNT_NAME"
+fi
+
+for mount in $(grep "KSU" /proc/mounts | awk {'print $2'}) ; do
+	/data/adb/ksud add-try-umount $mount
+done
+}
+
+# uncomment hiding method you want
+
+# do_ksud_hide
+# do_susfs_hide
 
 # EOF
+
