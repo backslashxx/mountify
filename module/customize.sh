c2a8f262dc531d2b29a7a2e683c519e7709c5789
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-10 09:54:41 +0800

    scripts/customize: block installation on susfs 1.5.10 / 1.5.11
    lots of users are reporting issues with it.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- module/customize.sh
+++ module/customize.sh
@@ -113,6 +113,17 @@ if { [ "$KSU" = true ] && [ ! "$KSU_MAGIC_MOUNT" = true ]; } || { [ "$APATCH" = 
 	echo "[!] This setup can cause issues and is NOT recommended."
 	echo "[!] modify customize.sh to force installation!"
 	abort "[!] Installation aborted!"
+	# ^ just change abort to echo or something
+fi
+
+SUSFS_BIN="/data/adb/ksu/bin/ksu_susfs"
+if [ "$KSU" = true ] && [ -f ${SUSFS_BIN} ] && [ "$( ${SUSFS_BIN} show version | head -n1 | sed 's/v//; s/\.//g' )" -ge 1510 ]; then
+	printf "\n\n"
+	echo "[!] ERROR: Mountify causes conflicts with this susfs version."
+	echo "[!] This setup can cause issues and is NOT recommended."
+	echo "[!] modify customize.sh to force installation!"
+	abort "[!] Installation aborted!"
+	# ^ just change abort to echo or something
 fi
 
 # EOF
