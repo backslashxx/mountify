0cf403a57020a0d2a84ac31330a8e038ea2a6de1
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-11 19:43:24 +0800

    LKM: remove kprobe requirement
    this can be done like how KernelSU does it.
    we just tell the LKM where the function is.
    
    ref:
    https://github.com/Kernel-SU/ksuinit/blob/master/src/loader.rs
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- nuke_ext4_lkm/README.md
+++ nuke_ext4_lkm/README.md
@@ -4,11 +4,20 @@ A small LKM to unregister an ext4 sysfs node.
 
 Usage:
 
-`insmod nuke.ko mount_point=/mnt/vendor/mountify`
+```shell
+#!/bin/sh
+
+kptr_set=$(cat /proc/sys/kernel/kptr_restrict)
+echo 1 > /proc/sys/kernel/kptr_restrict
+ptr_address=$(grep ext4_unregister_sysfs /proc/kallsyms | awk {'print "0x"$1'})
+insmod nuke.ko mount_point="/data/adb/modules" symaddr="$ptr_address"
+echo $kptr_set > /proc/sys/kernel/kptr_restrict
+```
 
 Compatibility:
 - Linux 4.4 ~ 6.17.
-
+- CONFIG_KALLSYMS=y
 
 
 From: KernelSU - [de29115](https://github.com/tiann/KernelSU/commit/de291151f1c2bd63cae1f797d938bfb14cbf2dc0)
+
--- nuke_ext4_lkm/nuke.c
+++ nuke_ext4_lkm/nuke.c
@@ -5,12 +5,26 @@
 #include <linux/namei.h>
 #include <linux/string.h>
 #include <linux/version.h>
+#include <linux/kallsyms.h>
 
 #ifndef MODULE
 #error "This is for LKM builds only. Do not compile built-in (CONFIG_NUKE_EXT4_SYSFS=y). Its bullshit."
 #endif
 
-// insmod nuke.ko mount_point=/data/adb/modules
+/* 
+ * USAGE:
+ *
+ * kptr_set=$(cat /proc/sys/kernel/kptr_restrict)
+ * echo 1 > /proc/sys/kernel/kptr_restrict
+ * ptr_address=$(grep ext4_unregister_sysfs /proc/kallsyms | awk {'print "0x"$1'})
+ * insmod nuke.ko mount_point="/data/adb/modules" symaddr="$ptr_address"
+ * echo $kptr_set > /proc/sys/kernel/kptr_restrict
+ * 
+ */
+
+static unsigned long symaddr;
+module_param(symaddr, ulong, 0000);
+MODULE_PARM_DESC(symaddr, "ext4_unregister_sysfs symbol address");
 
 static char *mount_point = "/data/adb/modules";
 module_param(mount_point, charp, 0000);
@@ -18,66 +32,39 @@ MODULE_PARM_DESC(mount_point, "nuke an ext4 sysfs node");
 
 static void __exit nuke_exit(void) {}
 
-// on 5.7, kallsyms_lookup_name is no longer exported
-// https://elixir.bootlin.com/linux/v5.7-rc1/source/kernel/kallsyms.c#L177
-// since most users are on GKI and kprobes likely work and enabled by default
-// we can use this kp.addr pullout trick
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
-#include <linux/kprobes.h>
 static int ext4_unregister_sysfs_fn(struct super_block *sb) 
 {
-	// pullout via kp.addr
-	struct kprobe kp = { .symbol_name = "ext4_unregister_sysfs", };
 	void (*ext4_unregister_sysfs_ptr)(struct super_block *);
+	const char *sym = "ext4_unregister_sysfs";
+	char buf[32] = {0};
 
-	int ret = register_kprobe(&kp);
-	if (ret) {
-		pr_info("nuke_ext4: register_kprobe failed: %d\n", ret);
+	if (!symaddr) {
+		pr_info("nuke_ext4: symaddr not provided!\n");
+		return -EINVAL;
+	}
+
+	// https://elixir.bootlin.com/linux/v6.17.1/source/kernel/kallsyms.c#L474
+	// turns out we can confirm the symbol!
+	sprint_symbol(buf, symaddr);
+	buf[31] = '\0';
+
+	// if strstarts symbol
+	// output is like "ext4_unregister_sysfs+0x0/0x70"
+	if (!!strncmp(buf, sym, strlen(sym))) {
+		pr_info("nuke_ext4: wrong symbol!? %s found!\n", buf);
 		return -EAGAIN;
 	}
 
-	if (!kp.addr) {
-		pr_info("nuke_ext4: ext4_unregister_sysfs not found!\n");
-		unregister_kprobe(&kp);
-		return -EAGAIN;
-	}
-
-	ext4_unregister_sysfs_ptr = (void (*)(struct super_block *))kp.addr;
-	pr_info("nuke_ext4: ext4_unregister_sysfs found on 0x%p\n", ext4_unregister_sysfs_ptr);
-
-	ext4_unregister_sysfs_ptr(sb);
-
-	unregister_kprobe(&kp);
-	return 0;
-
-}
-#else
-#include <linux/kallsyms.h>
-static int ext4_unregister_sysfs_fn(struct super_block *sb) 
-{
-	// pullout via kallsyms_lookup_name
-	void (*ext4_unregister_sysfs_ptr)(struct super_block *);
-	unsigned long addr = kallsyms_lookup_name("ext4_unregister_sysfs");
-	if (addr)
-		pr_info("nuke_ext4: ext4_unregister_sysfs found on 0x%lx\n",addr);
-	else {
-		pr_info("nuke_ext4: kallsyms_lookup_name failed for ext4_unregister_sysfs\n");
-		return -EAGAIN;
-	}
-	// cast to its actual fn type since kallsyms_lookup_name returns long address
-	// extern void ext4_unregister_sysfs(struct super_block *sb); on fs/ext4/ext4.h
-	// now I have no idea how kernelsu uses it as is, this fn is not exported for LKM
-	ext4_unregister_sysfs_ptr = (void (*)(struct super_block *))addr;
+	pr_info("nuke_ext4: sprint_symbol 0x%lx: %s\n", symaddr, buf);
+	ext4_unregister_sysfs_ptr = (void (*)(struct super_block *))symaddr;
 	ext4_unregister_sysfs_ptr(sb);
 	return 0;
 }
-#endif
 
-static int __init nuke_entry(void)
+static int __init nuke_entry(void) 
 {
 	struct path path;
-
-	pr_info("nuke_ext4: init with mount_point=%s\n", mount_point);
+	pr_info("nuke_ext4: init with symaddr: 0x%lx mount_point: %s\n", symaddr, mount_point);
 
 	// kang from ksu
 	int err = kern_path(mount_point, 0, &path);
@@ -109,7 +96,6 @@ static int __init nuke_entry(void)
 		path_put(&path);
 	} else
 		pr_info("nuke_ext4: procfs node nuked (%s is gone)\n", procfs_path);
-
 
 	pr_info("nuke_ext4: unload\n");
 	return -EAGAIN;
