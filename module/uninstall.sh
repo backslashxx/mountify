b924f102ecece1b01e6b5a2b12a5fdccf196181f
KOWX712 <leecc0503@gmail.com>
2025-10-21 12:14:31 +0800

    scripts/uninstall: remove flags create by moutnify webui
    #14
    
    Signed-off-by: KOWX712 <leecc0503@gmail.com>

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -5,10 +5,20 @@
 # No rights reserved.
 # This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
 PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
+FLAGS="/data/adb/ksu/.nomount
+/data/adb/ksu/.notmpfs
+/data/adb/.litemode_enable"
 
 # remove skip_mount on modules we skip_mounted
 for module in $(cat /data/adb/modules/mountify/skipped_modules) ; do 
 	rm /data/adb/modules/"$module"/skip_mount > /dev/null 2>&1
+done
+
+# remove flags created by mountify webui
+for flag in $FLAGS; do
+	if [ -f "$flag" ] && grep -q "mountify" "$flag"; then
+		rm -f "$flag"
+	fi
 done
 
 # EOF
