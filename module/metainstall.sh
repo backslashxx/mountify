3a65094d1084dcd5453ae043f74c575a6272a28e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-20 18:01:12 +0800

    scripts/metainstall: no-op handle_partition instead
    this is simpler
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- module/metainstall.sh
+++ module/metainstall.sh
@@ -20,28 +20,15 @@ mark_replace() {
 	chmod 644 $1
 }
 
-# undo_handle_partition
+# we no-op handle_partition
 # because ksu moves them e.g. MODDIR/system/product to MODDIR/product
-# this way we can support normal hierarchy that ksu breaks
-undo_handle_partition() {
-	partition_to_undo="$1"
-	if [ -L "$MODPATH/system/$partition_to_undo" ] && [ -d "$MODPATH/$partition_to_undo" ]; then
-		# ui_print "- undo handle_partition for /$partition_to_undo"
-		rm -f "$MODPATH/system/$partition_to_undo"
-		mv -f "$MODPATH/$partition_to_undo" "$MODPATH/system/$partition_to_undo"
-	fi
+# this way we can support normal hierarchy that ksu changes
+handle_partition() {
+	echo 0 > /dev/null ; true
 }
 
 # call install function, this is important!
 install_module
-
-# Run for typical partitions
-undo_handle_partition vendor
-undo_handle_partition product
-undo_handle_partition system_ext
-undo_handle_partition odm
-
-# ui_print "- mountify: undo handle_partition"
 
 # EOF
 
